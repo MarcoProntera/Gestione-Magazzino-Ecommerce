a3189ad04a22faca721f7e80a6dc0e6feb8e6c6a
lucaAntonini <luca.antonini@studenti.unicam.it>
2017-08-31 17:15:54 +0200

    g_menu fatto

--- public/javascripts/ajaxCall.js
+++ public/javascripts/ajaxCall.js
@@ -1,16 +1,21 @@
 function ajaxCall(type, url, timeout, data, success) {
     $('#overlay').fadeIn();
+    //AJAX CALL to say data to you
     $.ajax({
         type: type,
         url: url,
-        timeout: 10000,//timeout,
+        timeout: 20000,//timeout,
         data: data,
         success: function (data) {
             success(data);
-            $('#overlay').fadeOut();
+            $('#overlay').fadeOut(500);
         },
-        error: function(jqXHR, textStatus, err){
+        error: function (jqXHR, textStatus, err) {
             alert('text status ' + textStatus + ', err ' + err);
         }
     });
 }
+var socket;
+$(document).ready(function () {
+    socket = io.connect(window.location.origin.split(':')[0] + ':' + window.location.origin.split(':')[1] + ':3001');
+});
