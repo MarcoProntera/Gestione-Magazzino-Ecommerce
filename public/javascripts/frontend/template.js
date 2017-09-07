317db6389223d524f29ab3a19fb43ebc8b22f28d
Marco <marco.prontera@studenti.unicam.it>
2017-09-07 09:42:37 +0200

    fix pagina template frontend

--- public/javascripts/frontend/template.js
+++ public/javascripts/frontend/template.js
@@ -1,14 +1,22 @@
-function openLeftMenu() {
+function openLeftMenu(codiceMenu) {
+    prepareLeftMenu(codiceMenu);
     document.getElementById("main").style.marginLeft = "25%";
     document.getElementById("mySidebar").style.width = "25%";
     document.getElementById("mySidebar").style.display = "block";
+}
 
+function prepareLeftMenu(codiceMenu) {
+    $('#mySidebar').html('');
+    for (var i = 0; i < categorie.length; i++) {
+        if (categorie[i].codice_menu == codiceMenu) {
+            $('#mySidebar').append('<a href="/categoria?cat=' + categorie[i].codice + '" onclick="$(\'#overlay\').fadeIn();" class="inner-bar-item large all-width text-left"><span class="sub-left-menu-name">' + categorie[i].nome + '</span></a>');
+        }
+    }
 }
 
 function closeLeftMenu() {
     document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("mySidebar").style.display = "none";
-
 }
 
 function openRightMenu() {
@@ -27,21 +35,13 @@ function closeAllMenu() {
 function menuScrollCheck() {
     $(window).scroll(function () {
         var scroll = $('#barMenu').outerHeight(true) - $(this).scrollTop();
-
         if (scroll < 1) {
-            $('#mySidebar').css({
-                'top': '0'
-            });
+            $('#mySidebar').css({ 'top': '0' });
         } else {
-            $('#mySidebar').css({
-                'top': scroll
-            });
+            $('#mySidebar').css({ 'top': scroll });
         }
     });
 }
-$(document).ready(function () {
-    menuScrollCheck();
-});
 
 function clickMenu(sender) {
     $(sender).closest('div').find('button').each(function () {
@@ -61,7 +61,10 @@ function clickMenu(sender) {
         $('#mySidebar').addClass('menu_' + codice);
         if (!$(this).hasClass('button-active'))
             $(sender).addClass('button-clicked');
-        openLeftMenu();
+        openLeftMenu(codice);
     }
-
 }
+
+$(document).ready(function () {
+    menuScrollCheck();
+});
--- routes/frontend.js
+++ routes/frontend.js
@@ -5,49 +5,84 @@ var monGlo = require('zzCustom/mongoGlobal');
 var ObjectID = require("mongodb").ObjectID;
 
 /* HOME */
-router.get('/', function(req, res, next) {
-    monGlo.find('prodotti', {}, { nome: 1 }, function(search_result) {
-        res.render('template', { title: 'home', contenuto: 'categoria', prodotti: search_result });
+router.get('/', function (req, res, next) {
+    monGlo.find('menu', {}, { codice: 1 }, function (dati_menu) {
+        monGlo.find('categorie', {}, { codice_menu: 1 }, function (dati_categorie) {
+            monGlo.find('prodotti', {}, { nome: 1 }, function (search_result) {
+                res.render('template', { title: 'home', contenuto: 'categoria', menu: dati_menu, categorie: dati_categorie, prodotti: search_result, menuattivo: null });
+            });
+        });
     });
 });
 /* HOME */
 
 /* REGISTRAZIONE */
-router.get('/registrazione', function(req, res, next) {
-    res.render('template', { title: 'registrazione', contenuto: 'registrazione' });
+router.get('/registrazione', function (req, res, next) {
+    monGlo.find('menu', {}, { codice: 1 }, function (dati_menu) {
+        monGlo.find('categorie', {}, { codice_menu: 1 }, function (dati_categorie) {
+            monGlo.find('prodotti', {}, { nome: 1 }, function (search_result) {
+                res.render('template', { title: 'registrazione', contenuto: 'registrazione', menu: dati_menu, categorie: dati_categorie, prodotti: search_result, menuattivo: null });
+            });
+        });
+    });
 });
 /* REGISTRAZIONE */
 
 /* CATEGORIA */
-router.get('/categoria', function(req, res, next) {
+router.get('/categoria', function (req, res, next) {
     var codice_categoria = req.query.cat;
-    monGlo.find('categorie', { codice: Number(codice_categoria) }, {}, function(cat_search_result) {
-        monGlo.find('prodotti', { codice_categoria: Number(cat_search_result[0].codice) }, { nome: 1 }, function(search_result) {
-            res.render('template', { title: cat_search_result.nome, contenuto: 'categoria', prodotti: search_result });
+    monGlo.find('menu', {}, { codice: 1 }, function (dati_menu) {
+        monGlo.find('categorie', {}, { codice_menu: 1 }, function (dati_categorie) {
+            monGlo.find('categorie', { codice: Number(codice_categoria) }, {}, function (cat_search_result) {
+                monGlo.find('prodotti', { codice_categoria: Number(cat_search_result[0].codice) }, { nome: 1 }, function (search_result) {
+                    res.render('template', { title: cat_search_result.nome, contenuto: 'categoria', menu: dati_menu, categorie: dati_categorie, prodotti: search_result, menuattivo: cat_search_result[0].codice_menu });
+                });
+            });
         });
     });
 });
 /* CATEGORIA */
 
 /* PRODOTTO */
-router.get('/prodotto', function(req, res, next) {
+router.get('/prodotto', function (req, res, next) {
     //Titolo = nome del prodotto
-    res.render('template', { title: 'prodotto', contenuto: 'prodotto' });
+    var codice_prodotto = req.query.pro;
+    monGlo.find('menu', {}, { codice: 1 }, function (dati_menu) {
+        monGlo.find('categorie', {}, { codice_menu: 1 }, function (dati_categorie) {
+            monGlo.find('prodotti', { codice: Number(codice_prodotto) }, {}, function (dati_prodotto) {
+                monGlo.find('categorie', { codice: Number(dati_prodotto.codice_categoria) }, {}, function (cat_search_result) {
+                    res.render('template', { title: 'prodotto', contenuto: 'prodotto', menu: dati_menu, categorie: dati_categorie, dati_prodotto: dati_prodotto, menuattivo: cat_search_result[0].codice_menu });
+                });
+            });
+        });
+    });
 });
 /* PRODOTTO */
 
 /* CARRELLO */
-router.get('/carrello', function(req, res, next) {
-    res.render('template', { title: 'carrello', contenuto: 'carrello' });
+router.get('/carrello', function (req, res, next) {
+    monGlo.find('menu', {}, { codice: 1 }, function (dati_menu) {
+        monGlo.find('categorie', {}, { codice_menu: 1 }, function (dati_categorie) {
+            res.render('template', { title: 'carrello', contenuto: 'carrello', menu: dati_menu, categorie: dati_categorie, menuattivo: null });
+        });
+    });
 });
 /* CARRELLO */
 
 /* PROFILO */
-router.get('/profilo', function(req, res, next) {
-    res.render('template', { title: 'il mio profilo', contenuto: 'profilo', contenuto_sub: 'datiutente' });
+router.get('/profilo', function (req, res, next) {
+    monGlo.find('menu', {}, { codice: 1 }, function (dati_menu) {
+        monGlo.find('categorie', {}, { codice_menu: 1 }, function (dati_categorie) {
+            res.render('template', { title: 'il mio profilo', contenuto: 'profilo', contenuto_sub: 'datiutente', menu: dati_menu, categorie: dati_categorie, menuattivo: null });
+        });
+    });
 });
-router.get('/profilo/storicoordini', function(req, res, next) {
-    res.render('template', { title: 'il mio profilo', contenuto: 'profilo', contenuto_sub: 'storicoordini' });
+router.get('/profilo/storicoordini', function (req, res, next) {
+    monGlo.find('menu', {}, { codice: 1 }, function (dati_menu) {
+        monGlo.find('categorie', {}, { codice_menu: 1 }, function (dati_categorie) {
+            res.render('template', { title: 'il mio profilo', contenuto: 'profilo', contenuto_sub: 'storicoordini', menu: dati_menu, categorie: dati_categorie, menuattivo: null });
+        });
+    });
 });
 /* PROFILO */
 
